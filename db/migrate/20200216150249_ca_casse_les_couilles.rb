f46fb365995bd6d9bab953b37e48a48e6dc57610
Jessica Scianimanica <sciani.jessica@gmail.com>
2020-02-16 16:08:34 +0100

    damn

--- db/migrate/20200216150249_ca_casse_les_couilles.rb
+++
@@ -1,7 +0,0 @@
-class CaCasseLesCouilles < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :companies, :accounting_software, :text
-    remove_column :companies, :bank, :text
-    remove_column :companies, :payment, :text
-  end
-end
