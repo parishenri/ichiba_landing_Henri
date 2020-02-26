2934d80f67874697000b1954c13ce2add52da240
Henri Paris <paris_henri@yahoo.fr>
2020-02-26 23:43:05 +0100

    Test

--- db/migrate/20200216150824_add_columns_last_try.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsLastTry < ActiveRecord::Migration[5.2]
-  def change
-    add_column :companies, :accounting_software, :text
-    add_column :companies, :bank, :text
-    add_column :companies, :payment, :text
-    add_column :companies, :other_software, :text
-  end
-end
